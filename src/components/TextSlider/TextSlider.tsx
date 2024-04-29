977d2126d5e979b00e9796e0072ccd7361505c97
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-04-29 21:00:54 +0300

    feat: infinite animation

--- src/components/TextSlider/TextSlider.tsx
+++ src/components/TextSlider/TextSlider.tsx
@@ -6,10 +6,14 @@ interface ITextSlider {
 
 export const TextSlider: React.FC<ITextSlider> = ({ classes = "" }) => {
   return (
-    <div className={`${style.TextSlider} ${classes}`}>
-      <div className={`${style.block}`}>
-        English is a West Germanic language in the Indo-European language family, whose speakers, called Anglophones, originated in early medieval
-        England.
+    <div className={`${style.TextSlider} ${classes} `}>
+      <div className={`${style.ticker_wrap}`}>
+        <div className={`${style.ticker}`}>
+          <div className={`${style.ticker__item}`}>
+            Exploring trends and experimenting with <span>innovative</span> formats, our approach tackles ambitious business challenges, delights
+            audiences, and transforms consumer experiences for our clients.
+          </div>
+        </div>
       </div>
     </div>
   );
