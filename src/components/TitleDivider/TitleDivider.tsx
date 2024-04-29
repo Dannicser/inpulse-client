e7e9461442f440a92007adeea4b96199e18a5837
qwavy <nursultanr585@gmail.com>
2024-04-30 02:51:06 +0500

    feat line

--- src/components/TitleDivider/TitleDivider.tsx
+++ src/components/TitleDivider/TitleDivider.tsx
@@ -10,13 +10,17 @@ interface ITitleDivider {
 export const TitleDivider: React.FC<ITitleDivider> = ({ classes = "" }) => {
   return (
     <div className={`${style.TitleDividerWrapper} flex justify-center ${classes}`}>
+
       <div className={`${style.TitleDivider} w-[33.125rem] flex justify-center flex-col`}>
         <div className={`${style.title} text-4xl font-bold ml-auto mb-2 font-RoadRadio`}>REDEFINE BRAND</div>
-        <img className={`${style.decoration} w-[20.813rem] h-[3.125rem] `} src={decoration} alt="" />
+        <img className={`${style.decoration} w-[20.813rem] h-[3.125rem] `} src={decoration} alt=""/>
         <div className={`${style.orange}`}>
-          <img src={orange} alt="" />
+          <img src={orange} alt=""/>
         </div>
+
         <div className={`${style.title} text-4xl font-bold font-RoadRadio`}>EXPERIENCE</div>
+        <hr className={`${style.line}`}/>
+
       </div>
     </div>
   );
