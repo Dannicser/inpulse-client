890ff74c9f278bc06aaf09262c7170308d1ba19a
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-07-26 23:13:04 +0300

    build

--- src/components/SectionDo/SectionDo.tsx
+++ src/components/SectionDo/SectionDo.tsx
@@ -16,9 +16,10 @@ export const SectionDo: React.FC<ISectionDo> = ({ classes = "" }) => {
       <div className={`${style.do_inner}`}>
         <div className={`${style.do_inner_container}`}>
           <div className={`${style.do_title}`}>
-            <span className={`${style.above_title} font-light`}>{t("what")}</span>
+            <span className={`${style.above_title} font-light`}></span>
             <h3 className={`${style.title}`}>
-              {t("we")} <span className={`${style.red}`}> {t("do")}</span>?
+              {t("our")} <br />
+              <span className={`${style.red}`}> {t("mission")}</span>
             </h3>
           </div>
           <span className={`${style.do_hashtag}`}>
@@ -27,10 +28,10 @@ export const SectionDo: React.FC<ISectionDo> = ({ classes = "" }) => {
         </div>
         <div className={`${style.do_info} font-Involve font-medium`}>
           <p className={`${style.description}`}>
-            {t("do_text_up")}
+            {t("our_mission_text")}
             <br />
             <br />
-            <p className={`${style.grey}`}>{t("do_text_down")}</p>
+            {/* <p className={`${style.grey}`}>{t("do_text_down")}</p> */}
           </p>
         </div>
       </div>
